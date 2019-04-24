8e36383bd55341e75a13261f7069247ef732de1b
Soichi Hayashi <hayashis@iu.edu>
2019-04-24 12:53:20 -0400

    updated to include both hemi.

--- main.js
+++ main.js
@@ -12,25 +12,6 @@ if(!config) {
     }
 }
 
-/*
-Vue.component('prfview', {
-    name: 'prfview', 
-    props: [ "config" ],
-    data() {
-        return {
-        }
-    },
-    mounted() {
-        console.log("prfview mounted");
-    },
-    template: `
-        <div class="prfview">
-            hello
-        </div>
-    `,
-});
-*/
-
 new Vue({
     el: '#app',
     data: function() {
@@ -61,6 +42,9 @@ new Vue({
 
                 cortical_depth: 0,
                 inflate: 0,
+
+                split: 50,
+                open: Math.PI/4,
             },
 
             prf: {
@@ -112,7 +96,7 @@ new Vue({
 
         //camera light
         this.t.camera_light = new THREE.PointLight(0xffffff, 1);
-        this.t.camera_light.radius = 10;
+        //this.t.camera_light.radius = 1;
         this.t.scene.add(this.t.camera_light);
 
         this.t.controls = new THREE.OrbitControls(this.t.camera, this.t.renderer.domElement);
@@ -138,30 +122,41 @@ new Vue({
             let ui = this.gui.ui.addFolder('UI');
             ui.add(this.t.controls, 'autoRotate').listen();
             //ui.add(this.gui, 'show_stats');
-            ui.open();
-
-            var matrix = this.gui.ui.addFolder('Overlay');
-            matrix.add(this.gui, 'overlay', [ 'none', 'r2', 'r2*polar_angle', 'r2*rf_width', 'r2*eccentricity' ]).onChange(v=>{
-                console.log(v);
-                this.update_color();
-            });
-            matrix.add(this.gui, 'r2_min', 0, 0.1).step(0.001).onChange(v=>{
-                this.update_color();
-            });
-            matrix.add(this.gui, 'r2_max', 0, 10).onChange(v=>{
-                this.update_color();
-            });
-            matrix.open();
 
             ui.add(this.gui, 'cortical_depth', 0, 1).step(0.01).onChange(v=>{
                 if(!this.mesh.lh) return;
                 this.mesh.lh.morphTargetInfluences[0] = v;
+                this.mesh.rh.morphTargetInfluences[0] = v;
                 this.update_color();
             });
             ui.add(this.gui, 'inflate', 0, 1).step(0.01).onChange(v=>{
                 if(!this.mesh.lh) return;
                 this.mesh.lh.morphTargetInfluences[1] = v;
+                this.mesh.rh.morphTargetInfluences[1] = v;
             });
+            ui.add(this.gui, 'split', 0, 150).onChange(v=>{
+                if(!this.mesh.lh) return;
+                this.update_position();
+            });
+            ui.add(this.gui, 'open', 0, Math.PI).onChange(v=>{
+                if(!this.mesh.lh) return;
+                this.update_position();
+            });
+
+            ui.open();
+            
+            var overlay = this.gui.ui.addFolder('Overlay');
+            overlay.add(this.gui, 'overlay', [ 'none', 'r2', 'r2*polar_angle', 'r2*rf_width', 'r2*eccentricity' ]).onChange(v=>{
+                //console.log(v);
+                this.update_color();
+            });
+            overlay.add(this.gui, 'r2_min', 0, 0.5).step(0.01).onChange(v=>{
+                this.update_color();
+            });
+            overlay.add(this.gui, 'r2_max', 0, 2).step(0.01).onChange(v=>{
+                this.update_color();
+            });
+            overlay.open();
         },
 
         resized() {
@@ -178,99 +173,122 @@ new Vue({
         mousedown(event) {
         },
 
+        update_position() {
+            this.mesh.lh.position.x = -this.gui.split;
+            this.mesh.rh.position.x = this.gui.split;
+            this.mesh.lh.rotation.z = -this.gui.open;
+            this.mesh.rh.rotation.z = this.gui.open;
+        },
+
         update_color() {
             //make sure we have everything we need
             if(!this.mesh.lh) return;
+            if(!this.mesh.rh) return;
             if(!this.prf.header) return;
 
-            let geometry = this.mesh.lh.geometry;
-            let color = geometry.attributes.color;
-            let position = geometry.attributes.position;
-            color.needsUpdate = true;
-            
             //console.log("update_color");
             let v_data;
             let v_stats;
+            let r2_data;
             switch(this.gui.overlay) {
-            case "none":
-                console.log("none!");
-                for(var i = 0;i < color.count;++i) { 
-                    color.setXYZ(i, 255, 255, 255); 
-                }
-                return;
+            case "r2":
+                r2_data = this.prf.r2;
+                break;
             case "r2*polar_angle":
+                r2_data = this.prf.r2;
                 v_data = this.prf.p_angle;
                 v_stats = this.prf.p_angle_stats;
                 break;
             case "r2*rf_width":
+                r2_data = this.prf.r2;
                 v_data = this.prf.rf_width;
                 v_stats = this.prf.rf_width_stats;
                 break;
             case "r2*eccentricity":
+                r2_data = this.prf.r2;
                 v_data = this.prf.ecc;
                 v_stats = this.prf.ecc_stats;
                 break;
             }
 
-            for(var i = 0;i < color.count;++i) { 
-                //get vertex coord
-                let x = position.getX(i);
-                let y = position.getY(i);
-                let z = position.getZ(i);
-                
-                //convert it to voxel coords and get the value
-                let vx = Math.round((x - this.prf.header.spaceOrigin[0]) / this.prf.header.thicknesses[0]);
-                let vy = Math.round((y - this.prf.header.spaceOrigin[1]) / this.prf.header.thicknesses[1]);
-                let vz = Math.round((z - this.prf.header.spaceOrigin[2]) / this.prf.header.thicknesses[2]);
+            let lh_geometry = this.mesh.lh.geometry;
+            let lh_color = lh_geometry.attributes.color;
+            let lh_position = lh_geometry.attributes.position;
 
-                let r2 = this.prf.r2.get(vz, vy, vz);
-                if(isNaN(r2)) {
-                    color.setXYZ(i, 50, 50, 50); 
-                    continue;
-                }
-                //TODO - the way r2/min/max is applied is wrong
-                r2 = map_value(r2, this.prf.r2_stats.min, this.prf.r2_stats.max, this.gui.r2_min, this.gui.r2_max);
-                //if(i % 5000 == 0) console.log(r2);
+            let rh_geometry = this.mesh.rh.geometry;
+            let rh_color = rh_geometry.attributes.color;
+            let rh_position = rh_geometry.attributes.position;
+ 
+            function set_color(color, position) {
+                for(var i = 0;i < color.count;++i) { 
+                    if(!r2_data) {
+                        //must be none
+                        color.setXYZ(i, 200, 200, 200); 
+                        continue;
+                    }
+                    //get vertex coord
+                    let x = position.getX(i);
+                    let y = position.getY(i);
+                    let z = position.getZ(i);
+                    
+                    //convert it to voxel coords and get the value
+                    let vx = Math.round((x - this.prf.header.spaceOrigin[0]) / this.prf.header.thicknesses[0]);
+                    let vy = Math.round((y - this.prf.header.spaceOrigin[1]) / this.prf.header.thicknesses[1]);
+                    let vz = Math.round((z - this.prf.header.spaceOrigin[2]) / this.prf.header.thicknesses[2]);
 
-                if(v_data) {
-                    let v = v_data.get(vz, vy, vz);      
-                    if(isNaN(v)) {
+                    let r2 = r2_data.get(vz, vy, vz);
+                    if(isNaN(r2)) {
                         color.setXYZ(i, 50, 50, 50); 
                         continue;
                     }
-                    let h = map_value(v, v_stats.min, v_stats.max, 0, 180);
-                    let s = 1;
-                    let l = r2;
+                    //TODO - the way r2/min/max is applied is wrong
+                    r2 = map_value(r2, this.prf.r2_stats.min - this.gui.r2_min, this.gui.r2_max/this.prf.r2_stats.max, 0, 1);
 
-                    //convert hsl to rgb
-                    let c = (1 - Math.abs(2 * l - 1)) * s;
-                    let x = c * (1 - Math.abs((h / 60) % 2 - 1));
-                    let m = l - c/2;
-                    let r = 0;
-                    let g = 0;
-                    let b = 0;
-                    if (0 <= h && h < 60) {
-                        r = c; g = x; b = 0;
-                    } else if (60 <= h && h < 120) {
-                        r = x; g = c; b = 0;
-                    } else if (120 <= h && h < 180) {
-                        r = 0; g = c; b = x;
-                    } else if (180 <= h && h < 240) {
-                        r = 0; g = x; b = c;
-                    } else if (240 <= h && h < 300) {
-                        r = x; g = 0; b = c;
-                    } else if (300 <= h && h < 360) {
-                        r = c; g = 0; b = x;
+                    if(v_data) {
+                        let v = v_data.get(vz, vy, vz);      
+                        if(isNaN(v)) {
+                            color.setXYZ(i, 50, 50, 50); 
+                            continue;
+                        }
+                        let h = map_value(v, v_stats.min, v_stats.max, 0, 240);
+                        //if(i % 5000 == 0) console.log(v, h);
+                        let s = 1;
+                        let l = r2;
+
+                        //convert hsl to rgb
+                        let c = (1 - Math.abs(2 * l - 1)) * s;
+                        let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+                        let m = l - c/2;
+                        let r = 0;
+                        let g = 0;
+                        let b = 0;
+                        if (0 <= h && h < 60) {
+                            r = c; g = x; b = 0;
+                        } else if (60 <= h && h < 120) {
+                            r = x; g = c; b = 0;
+                        } else if (120 <= h && h < 180) {
+                            r = 0; g = c; b = x;
+                        } else if (180 <= h && h < 240) {
+                            r = 0; g = x; b = c;
+                        } else if (240 <= h && h < 300) {
+                            r = x; g = 0; b = c;
+                        } else if (300 <= h && h < 360) {
+                            r = c; g = 0; b = x;
+                        }
+                        r = Math.round((r + m) * 255);
+                        g = Math.round((g + m) * 255);
+                        b = Math.round((b + m) * 255);
+                        color.setXYZ(i, r, g, b);
+                    } else {
+                        //r2 only
+                        color.setXYZ(i, r2*255, 0, 0);
                     }
-                    r = Math.round((r + m) * 255);
-                    g = Math.round((g + m) * 255);
-                    b = Math.round((b + m) * 255);
-                    color.setXYZ(i, r, g, b);
-                } else {
-                    //r2 only
-                    color.setXYZ(i, r2*255, 0, 0);
                 }
             }
+            set_color.call(this, rh_color, rh_position);
+            set_color.call(this, lh_color, lh_position);
+            lh_color.needsUpdate = true;
+            rh_color.needsUpdate = true;
         },
 
         animate() {
@@ -303,59 +321,73 @@ new Vue({
             return {min, max};
         },
 
+        create_mesh(material, base_geometry, white_geometry, inflated_geometry) {
+            //first create a normal mesh
+            var mesh = new THREE.Mesh( base_geometry, material );
+            mesh.rotation.x = -Math.PI/2;
+            this.t.scene.add(mesh);
+    
+            //init colors for each vertices
+            let position = mesh.geometry.attributes.position;
+            let colors = new Uint8Array(position.count*3);
+            colors.fill(0, 0, position.count);
+            mesh.geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3, true).setDynamic(true));
+
+            //set as morph target
+            let mattr = mesh.geometry.morphAttributes;
+            mattr.position = [
+                white_geometry.attributes.position.clone(),
+                inflated_geometry.attributes.position.clone(),
+            ];
+            mattr.normal = [
+                white_geometry.attributes.normal.clone(),
+                inflated_geometry.attributes.normal.clone(),
+            ];
+            mesh.updateMorphTargets();
+            return mesh;
+        },
+
         load() {
             let vtkloader = new THREE.VTKLoader();
 
-            let lh_pial = new Promise((resolve, reject)=>{
-                vtkloader.load("testdata/lh.pial.vtk", resolve);
-            });
-            let lh_white = new Promise((resolve, reject)=>{
-                vtkloader.load("testdata/lh.white.vtk", resolve);
-            });
-            let lh_inflated = new Promise((resolve, reject)=>{
-                vtkloader.load("testdata/lh.inflated.vtk", resolve);
-            });
+            let vtks = [ 
+                "testdata/lh.pial.vtk",
+                "testdata/lh.white.vtk",
+                "testdata/lh.inflated.vtk",
 
-            console.log("loading..");
-            let all = Promise.all([lh_pial, lh_white, lh_inflated]).then(geometries=>{
-                console.log("done loading lh");
-                geometries.map(geometry=>geometry.computeVertexNormals());
-                //geometries.map(geometry=>geometry.computeMorphNormals());
-
-                let material = new THREE.MeshLambertMaterial({
-                    //color: new THREE.Color(0.5,0.8,1.0),
-                    vertexColors: THREE.VertexColors,
-                    //shininess: 80,
-                    morphTargets: true,
-                    morphNormals: true, //doesn't work?
+                "testdata/rh.pial.vtk",
+                "testdata/rh.white.vtk",
+                "testdata/rh.inflated.vtk",
+            ];
+            let promises = vtks.map(vtk=>{
+                return new Promise((resolve, reject)=>{
+                    vtkloader.load(vtk, resolve);
                 });
+            });
 
-                //first create a normal mesh
-                var mesh = new THREE.Mesh( geometries[0], material );
-                mesh.rotation.x = -Math.PI/2;
-                this.mesh.lh = mesh;
-                this.t.scene.add(mesh);
-        
-                //init colors for each vertices
-                let position = mesh.geometry.attributes.position;
-                let colors = new Uint8Array(position.count*3);
-                colors.fill(0, 0, position.count);
-                mesh.geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3, true).setDynamic(true));
+            console.log("loadin all vtks");
+            let all = Promise.all(promises).then(geometries=>{
+                geometries.map(geometry=>geometry.computeVertexNormals());
+
+                let lh_material = new THREE.MeshLambertMaterial({
+                    vertexColors: THREE.VertexColors,
+                    morphTargets: true,
+                    morphNormals: true, 
+                });
+                this.mesh.lh = this.create_mesh(lh_material, geometries[0], geometries[1], geometries[2]);
+                this.t.scene.add(this.mesh.lh);
+
+                let rh_material = new THREE.MeshLambertMaterial({
+                    vertexColors: THREE.VertexColors,
+                    morphTargets: true,
+                    morphNormals: true, 
+                });
+                this.mesh.rh = this.create_mesh(rh_material, geometries[3], geometries[4], geometries[5]);
+                this.t.scene.add(this.mesh.rh);
+                
+                console.log("loaded all vtks");
                 this.update_color();
-
-                //set as morph target
-                let mattr = mesh.geometry.morphAttributes;
-                mattr.position = [
-                    geometries[1].attributes.position.clone(), //lh_white
-                    geometries[2].attributes.position.clone(), //lh_inflated
-                ];
-                mattr.normal = [
-                    geometries[1].attributes.normal.clone(), //lh_white
-                    geometries[2].attributes.normal.clone(), //lh_inflated
-                ];
-                mesh.updateMorphTargets();
-
-                console.dir(mesh.geometry);
+                this.update_position();
             });
             
             /* I am not sure what I can use this for..
@@ -408,6 +440,8 @@ new Vue({
                     let N = nifti.parse(pako.inflate(buf));
                     this.prf.p_angle = ndarray(N.data, N.sizes.slice().reverse());
                     this.prf.p_angle_stats = this.compute_stats(N.data);
+                    //console.log("p_angle");
+                    //console.dir(this.prf.p_angle_stats);
                     resolve();
                 });
             });
@@ -420,6 +454,8 @@ new Vue({
                     let N = nifti.parse(pako.inflate(buf));
                     this.prf.rf_width = ndarray(N.data, N.sizes.slice().reverse());
                     this.prf.rf_width_stats = this.compute_stats(N.data);
+                    //console.log("rfWidth");
+                    //console.dir(this.prf.rf_width_stats);
                     resolve();
                 });
             });
@@ -437,6 +473,7 @@ new Vue({
             });
 
             Promise.all([ r2, p_angle, rf_width, ecc ]).then(()=>{
+                console.log("loaded all nifties");
                 this.update_color();
             });
         },
